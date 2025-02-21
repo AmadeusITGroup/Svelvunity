5ae9dd2d0227469dec326126952c1349f9978eba
Aroosa-Batool <aroocabatool@gmail.com>
2025-02-21 11:29:50 +0100

    fix: seperate vitest config

--- vite.config.ts
+++ vite.config.ts
@@ -2,16 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./src/setupTest.js'],
-		exclude: ['dist/**', '**/node_modules/**'],
-		coverage: {
-			provider: 'v8', 
-			reporter: ['text', 'lcov'],
-			exclude: ['**config.**', '**.eslintrc.**', '**/src/routes/**', '.svelte-kit/**', 'dist/**', '**/app.d.ts'],
-		}
-	}
+	plugins: [sveltekit()]
 });
