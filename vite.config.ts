466fd77ba8773dfd2adef561e741b36359f74754
Aroosa Batool <Aroocabatool@gmail.com>
2025-03-05 18:41:29 +0100

    fix: security patch (#3)
    * fix: security patch vitest upgrade

--- vite.config.ts
+++ vite.config.ts
@@ -2,15 +2,5 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
-	plugins: [sveltekit()],
-	test: {
-		globals: true,
-		environment: 'jsdom',
-		setupFiles: ['./src/setupTest.js'],
-		exclude: ['dist/**', '**/node_modules/**'],
-		coverage: {
-			reporter: ['text', 'lcov'],
-			exclude: ['**config.**', '**.eslintrc.**', '**/src/routes/**', '.svelte-kit/**']
-		}
-	}
+	plugins: [sveltekit()]
 });
